1e6a9f0a22c6aa6a3e3b5d7f94ab3dbb62b81b3b
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-03-28 22:14:03 +0200

    end home work 1

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,6 +1,15 @@
-export default function FriendList() {
+import clsx from "clsx";
+import FriendListItem from "../FriendListItem/FriendListItem"
+import css from "./FriendList.module.css"
+export default function FriendList({friends}) {
     return (
-      <>
-      </>
-    )
-  }
+<ul className={css.listItem}>
+	{friends.map((friend) => (
+  <li key={friend.id} className={css.item} >
+    <FriendListItem friend
+    ={friend} />
+    </li>
+    ))}
+</ul>
+ )
+}
